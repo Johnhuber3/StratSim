19cbf686660b659758399a691ee7fec007a495da
johnhuber3 <johnrhuber001@gmail.com>
2023-06-25 18:57:02 -0400

    Fixed page loading positon, Now I'm done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Navbar from "./Navbar";
 import Baccarat from './components/pages/Baccarat';
 import Roulette from './components/pages/Roulette';
@@ -7,17 +9,25 @@ import Home from './components/pages/Home';
 import { Route, Routes } from "react-router-dom";
 
 function App() {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Reset scroll position on component mount
+  }, [location]);
+
   return (
-  <><Navbar />
-  <div className="container">
-    <Routes>
-      <Route path="/"  element={<Home />} />
-      <Route path="/Roulette"  element={<Roulette />} />
-      <Route path="/Craps"  element={<Craps />} />
-      <Route path="/BlackJack"  element={<BlackJack />} />
-      <Route path="/Baccarat"  element={<Baccarat />} />
-    </Routes>
-  </div>
+  <>
+    <Navbar />
+    <div className="container">
+      <Routes>
+        <Route path="/"  element={<Home />} />
+        <Route path="/Roulette"  element={<Roulette />} />
+        <Route path="/Craps"  element={<Craps />} />
+        <Route path="/BlackJack"  element={<BlackJack />} />
+        <Route path="/Baccarat"  element={<Baccarat />} />
+      </Routes>
+    </div>
   </>)
 }
 
